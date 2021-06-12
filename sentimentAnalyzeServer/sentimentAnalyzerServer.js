9bb7b16205af3e492b5b2953522b51ec88cf72ef
vinnynarula1 <vinny.narula7@gmail.com>
2021-06-12 21:25:57 +0000

    changes made

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -6,6 +6,42 @@ app.use(express.static('client'))
 const cors_app = require('cors');
 app.use(cors_app());
 
+function getNLUInstance(){
+    let api_key = process.env.API_KEY;
+    let url_key = process.env.API_URL;
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    version: '2020-08-01',
+    authenticator: new IamAuthenticator({
+    apikey: api_key,
+  }),
+  serviceUrl: url_key,
+});
+
+const analyzeParams = {
+  'url': 'www.wsj.com/news/markets',
+  'features': {
+    'sentiment': {
+      'targets': [
+        'stocks'
+      ]
+    }
+  }
+};
+
+naturalLanguageUnderstanding.analyze(analyzeParams)
+  .then(analysisResults => {
+    console.log(JSON.stringify(analysisResults, null, 2));
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+
+
+}
+
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
